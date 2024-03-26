6d56f8b6c63f9554d41c51863bfc0e945690e158
Teguera <aboubacarteguera21@example.com>
2024-03-26 01:40:39 +0000

    destruction de poste

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const createPost = require('../controller/post/createPost');
 const updatePost = require('../controller/post/updatePost');
+const destroyPost = require('../controller/post/destroyPost');
 const postRouter = express.Router();
 
 postRouter.route('/create-post').post(createPost);
 postRouter.route('/update-post/:id').put(updatePost);
+postRouter.route('/destroy-post/:id').delete(destroyPost);
 
 module.exports = postRouter;
