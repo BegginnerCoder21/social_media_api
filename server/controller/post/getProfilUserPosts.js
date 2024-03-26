143cc9d8e238963952feb0e8e01ce35344f13038
Teguera <aboubacarteguera21@example.com>
2024-03-26 19:28:12 +0000

    fin du backend

--- server/controller/post/getProfilUserPosts.js
+++ server/controller/post/getProfilUserPosts.js
@@ -9,12 +9,12 @@ const getProfilUserPosts = async(req, res) => {
         const current_user = await userModel.findById(userId);
         const user_post = await postModel.find({userId: current_user._id});
         // console.log(user_post);
-        followers_post = await Promise.all(current_user.followers.map((followers_id) => {
+        followings_post = await Promise.all(current_user.followings.map((followings_id) => {
             // console.log(followers_id);
-            return postModel.find({userId: followers_id});
+            return postModel.find({userId: followings_id});
         }));
 
-        const posts = user_post.concat(followers_post);
+        const posts = user_post.concat(followings_post);
         // console.log(posts);
 
         return res.status(200).json({posts})
